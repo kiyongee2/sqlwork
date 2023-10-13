285ef06a2f09176fa7056c377cd85a1279b386fa
kiyongee2 <kiyongee2@gmail.com>
2023-10-13 18:13:19 +0900

    account table

--- mydb/boards.sql
+++ mydb/boards.sql
@@ -8,7 +8,7 @@ CREATE TABLE boards(
     bfilename VARCHAR2(50),
     bfiledata BLOB    -- BLOB(Binary Large Of Byte)
 );
--- 시퀀스 생성
+-- 시퀀스 생성(NOCACHE - 저장하지 않음, 초기화되면 1부터 시작함)
 CREATE SEQUENCE seq_bno NOCACHE;
 
 -- 게시글 추가
@@ -20,6 +20,21 @@ VALUES (seq_bno.NEXTVAL, 'smartPhone', '삼성 갤럭시 S21입니다', 'today',
             SYSDATE, null, null);
 
 COMMIT;
-
-SELECT * FROM boards WHERE bwriter = 'sky123'
+-- 전체 검색
+SELECT * FROM boards
 ORDER BY bno DESC;
+
+SELECT * FROM boards WHERE bwriter = 'cloud'
+ORDER BY bno DESC;
+
+-- 글번호 5번의 제목을 '아이폰 15'으로 변경
+UPDATE boards 
+SET btitle = '아이폰 15',
+    bcontent = '아이폰 15 제품입니다.'
+WHERE bno = 5;
+
+-- 1번 게시글 삭제
+DELETE FROM boards
+WHERE bno = 1;
+
+ROLLBACK;
