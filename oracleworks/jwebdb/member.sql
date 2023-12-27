7c7f2bd71fc4d72334313e9934d39b91d9f7f49e
kiyongee2 <kiyongee2@gmail.com>
2023-12-27 18:20:08 +0900

    spring user

--- oracleworks/jwebdb/member.sql
+++ oracleworks/jwebdb/member.sql
@@ -17,9 +17,21 @@ VALUES(seq_mno.NEXTVAL, 'khit', 'm1234567', '김기용', 'khit@gmail.com', '남');
 COMMIT;
 
 -- id가 'khit'인 회원의 정보
-SELECT * FROM member WHERE id = 'khit';
+SELECT * FROM member;
+
+-- id와 passwd가 일치하는 회원 찾기
+SELECT * FROM member 
+WHERE id = 'khit' and passwd = 'm1234567';
 
 SELECT * FROM member ORDER BY mno;
+
+-- result 별칭이지만 칼럼 역할을 함
+SELECT COUNT(*) as result
+FROM member WHERE id = 'khit';
+
+-- 아이디의 개수가 1이면 true, 아니면 false 출력
+SELECT DECODE(COUNT(*), 1, 'true', 'false') AS result
+FROM member WHERE id = 'khit';
 
 
 
